628512eda7914af44817aa1336818199243f9fcc
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-10-21 10:00:22 -0700

    fix rules imports

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const preferCustomHooks = require('./rules/prefer-custom-hooks')
-const noSkippedOrFocusedTests = require('./rules/no-skipped-or-focused-tests')
 
 module.exports = {
   rules: {
-    'no-skipped-or-focused-tests': noSkippedOrFocusedTests,
     'prefer-custom-hooks': preferCustomHooks,
   },
 }
